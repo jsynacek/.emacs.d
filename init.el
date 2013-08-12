00b0bab1c011ea264ef914114f447bd99ba559c6
Jan Synacek <jsynacek@redhat.com>
2013-08-12 08:53:41 +0200

    use packaged solazired theme

--- init.el
+++ init.el
@@ -6,16 +6,12 @@
 (setq user-nick "jsynacek")
 
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/emacs-color-theme-solarized/"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/ido-hacks"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/gitsum"))
 
 (require 'idomenu)
 
 (require 'ido-hacks)
-
-(require 'color-theme-solarized)
-(color-theme-solarized-dark)
 
 (require 'package)
 (add-to-list 'package-archives
@@ -34,6 +30,7 @@
                     highlight-symbol
                     magit
                     paredit
+                    solarized-theme
                     undo-tree
                     yasnippet)))
     (dolist (pkg pkg-list)
@@ -314,6 +311,8 @@
  '(backup-directory-alist (quote (("." . "/home/jsynacek/emacsbackup"))))
  '(column-number-mode t)
  '(confirm-kill-emacs nil)
+ '(custom-enabled-themes (quote (solarized-dark)))
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(ediff-diff-options "")
  '(electric-pair-mode nil)
  '(global-auto-revert-mode t)
