44a68ad445de3b7e6271cc0b103336e317ab5783
Jan Synacek <jsynacek@redhat.com>
2013-08-12 08:54:47 +0200

    improve magit

--- init.el
+++ init.el
@@ -251,6 +251,34 @@
       (error "No number at point"))
   (replace-match (number-to-string (1- (string-to-number (match-string 0))))))
 
+;; full screen magit-status
+(defadvice magit-status (around magit-fullscreen activate)
+   (window-configuration-to-register :magit-fullscreen)
+   ad-do-it
+   (delete-other-windows))
+
+(defun magit-quit-session ()
+   "Restores the previous window configuration and kills the magit buffer"
+   (interactive)
+   (kill-buffer)
+   (jump-to-register :magit-fullscreen))
+
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; keybindings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -302,6 +330,10 @@
 (global-set-key (kbd "M-i") 'idomenu)
 (global-set-key (kbd "C-M-/") 'hippie-expand-line)
 (global-set-key (kbd "M-w") 'save-region-or-current-line)
+
+(require 'magit)
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; customized
