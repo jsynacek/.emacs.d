61b8beb6220cad8f0a54839bcfb35501a82e7e68
Jan Synacek <jsynacek@redhat.com>
2014-01-21 10:37:18 +0100

    bind org-iswitchb

--- setup-org.el
+++ setup-org.el
@@ -47,6 +47,7 @@ Use favorite font instead. Default is `jsynacek/font', if bound, otherwise
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
 
 ;; (add-hook 'org-mode-hook 'org-indent-mode)
 
