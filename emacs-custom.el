0d8983be740ac962cee4a39e56ae5116f6c49772
Jan Synacek <jsynacek@redhat.com>
2014-04-18 14:36:34 +0200

    don't hide symlink targets in dired

--- emacs-custom.el
+++ emacs-custom.el
@@ -14,6 +14,7 @@
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(diff-switches "-u")
  '(dired-dwim-target t)
+ '(dired-hide-details-hide-symlink-targets nil)
  '(dired-listing-switches "-al --group-directories-first")
  '(dired-use-ls-dired t)
  '(ediff-custom-diff-options "-up")
