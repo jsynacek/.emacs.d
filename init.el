0b0d6091e7e39df95da7ad57ac38638feb8a6d5b
Jan Synáček <jan.synacek@scrive.com>
2022-04-08 13:46:59 +0200

    Make my own theme

--- init.el
+++ init.el
@@ -19,54 +19,7 @@
                             (font . "Liberation Mono 11")))
 ;;(set-default 'cursor-type  '(bar . 2))
 ;;(set-window-margins (selected-window) 10)
-
-(load (concat user-emacs-directory "open-color.el"))
-(face-spec-set 'default
-               `((t :foreground ,(oc-color 'gray9)
-                    ;; :background "#f8f9fa"))) ; gray1
-                    :background "white")))
-(face-spec-set 'fringe
-               '((t :background "#f8f9fa"))) ; gray1
-(face-spec-set 'region
-               '((t :background "#e9ecef"))) ; gray2
-(face-spec-set 'font-lock-comment-face
-               '((t :foreground "#868e96"))) ; gray6
-(face-spec-set 'font-lock-doc-face
-               '((t :inherit font-lock-comment-face)))
-(face-spec-set 'font-lock-string-face
-               '((t :foreground "#c92a2a"))) ; red9
-(face-spec-set 'font-lock-keyword-face
-               '((t :foreground "#1c7ed6"))) ; blue7
-(face-spec-set 'font-lock-builtin-face
-               '((t :foreground nil)))
-(face-spec-set 'font-lock-type-face
-               '((t :foreground nil)))
-(face-spec-set 'font-lock-constant-face
-               '((t :foreground nil)))
-(face-spec-set 'font-lock-variable-name-face
-               '((t :foreground nil)))
-(face-spec-set 'font-lock-function-name-face
-               '((t :foreground nil
-                    :inherit font-lock-keyword-face)))
-(face-spec-set 'mode-line
-               '((t :foreground "#212529"    ; gray9
-                    :background "#ced4da"    ; gray4
-                    :box (:style flat-button))))
-(face-spec-set 'mode-line-inactive
-               '((t :foreground "#212529"    ; gray9
-                    :background "#e9ecef"    ; gray2
-                    :box (:style flat-button))))
-(face-spec-set 'mode-line-highlight
-               '((t :foreground "#212529"    ; gray9
-                    :background "#dee2e6"    ; gray3
-                    :box (:style flat-button))))
-
-(face-spec-set 'show-paren-match
-               '((t :foreground nil
-                    :background "#d3f9d8"))) ; green1
-(face-spec-set 'show-paren-mismatch
-               '((t :foreground nil
-                    :background "#ffe3e3"))) ; red1
+(load (concat user-emacs-directory "jsynacek-theme.el"))
 
 ;;; packages
 (require 'package)
