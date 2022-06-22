8c8d65cad40a9a682539afd115316604d20d352b
Jan Synáček <jan.synacek@scrive.com>
2022-06-22 10:10:13 +0200

    haskell-lite: Add syntax highlighting for type annotation

--- lisp/haskell-lite.el
+++ lisp/haskell-lite.el
@@ -5,6 +5,12 @@
   "Font lock face for CPP conditionals in Haskell source code.")
 (defvar haskell-cpp-conditional-face 'haskell-cpp-conditional-face)
 
+(defface haskell-type-annotation-face
+  `((t :foreground ,(oc-color 'cyan7)
+       :weight bold))
+  "Font lock face for type annotations in Haskell source code.")
+(defvar haskell-type-annotation-face 'haskell-type-annotation-face)
+
 (define-skeleton skel/haskell-language-pragma "Insert haskell LANGUAGE pragma"
   (completing-read
    "Extension: "
@@ -27,8 +33,10 @@
               (let* ((cpp-rx (rx line-start
                                  "#" (or "if" "ifdef" "else" "elif" "endif")
                                  word-boundary))
+                     (type-annotation-rx "::")
                      (keywords
-                      `((,cpp-rx . haskell-cpp-conditional-face))))
+                      `((,cpp-rx . haskell-cpp-conditional-face)
+                        (,type-annotation-rx . haskell-type-annotation-face))))
                 (list keywords))))
 
 (add-to-list 'auto-mode-alist '("\\.hs\\'" . haskell-lite-mode))
